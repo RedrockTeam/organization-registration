57f8f19c5c95c2358c7f4e45ee09097adffab411
lin0102 <1262280723@qq.com>
2019-08-22 02:36:48 +0800

    完成首页逻辑

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,20 +3,88 @@ import { View, Image, Button, Text } from '@tarojs/components'
 import './index.scss'
 import titlepng from '../../assets/title2x.png'
 import sloganpng from '../../assets/slogan2x.png'
+import api from '../../api'
+import formatQuery from '../../utils/formatQuery'
 
-export default class Index extends PureComponent {
+interface StuInfo {
+  stu_name: string
+  stu_num: string
+  stu_qq: string
+  stu_phone: string
+}
 
+interface State {
+  enableClick: boolean
+  clickType: string
+  stuInfo: StuInfo
+}
+
+export default class Index extends PureComponent<{}, State> {
   config: Config = {
-    navigationStyle: 'custom',
+    navigationStyle: 'custom'
   }
 
-  toEntranceInfo() {
-    Taro.redirectTo({
-      url: '/pages/info-entrance/index'
+  state = {
+    enableClick: false,
+    clickType: '',
+    stuInfo: {
+      stu_name: '',
+      stu_num: '',
+      stu_qq: '',
+      stu_phone: ''
+    }
+  }
+
+  beginRegister() {
+    if (!this.state.enableClick) {
+      return
+    }
+    const clickType = this.state.clickType
+    if (clickType === 'toInfo') {
+      Taro.navigateTo({
+        url: '/pages/info-entrance/index'
+      })
+    } else if (clickType === 'toIndex') {
+      const stuInfo = this.state.stuInfo
+      Taro.navigateTo({
+        url: `/pages/main/index${formatQuery(stuInfo)}`
+      })
+    }
+    Taro.showLoading({
+      title: '加载中...'
     })
+    setTimeout(() => {
+      Taro.hideLoading()
+    }, 2000)
   }
 
-  componentWillMount() {}
+  async login() {
+    const loginInfo = await Taro.login()
+    console.log(loginInfo)
+
+    if (loginInfo && loginInfo.code) {
+      const code = loginInfo.code
+      const info = await api.getOpenid(code)
+      if (info.status === 202) {
+        this.setState({
+          enableClick: true,
+          clickType: 'toInfo'
+        })
+      } else if (info.status === 200) {
+        this.setState({
+          enableClick: true,
+          clickType: 'toIndex',
+          stuInfo: info.data
+        })
+      } else {
+        console.log(info)
+      }
+    }
+  }
+
+  componentWillMount() {
+    // this.login()
+  }
 
   componentDidMount() {}
 
@@ -31,7 +99,7 @@ export default class Index extends PureComponent {
       <View className="index">
         <Image src={titlepng} />
         <Image src={sloganpng} />
-        <Button onClick={this.toEntranceInfo}>开始报名</Button>
+        <Button onClick={this.beginRegister}>开始报名</Button>
         <Text>第一次登陆需绑定信息哦</Text>
       </View>
     )
