380b81546051d41a846ff2cdb16cc21d1f755d3f
lin0102 <1262280723@qq.com>
2019-08-29 21:43:18 +0800

    报名页

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -4,34 +4,23 @@ import { View, Image, Button, Text } from '@tarojs/components'
 import Navigation from '../../components/Navigation'
 import Mask from '../../components/Mask'
 
+import { StuInfoContext } from '../../data/context'
+import api from '../../api'
+
 import './index.scss'
 
-interface Info {
-  stu_name: string
-  stu_num: string
-  stu_qq: string
-  stu_phone: string
-  department: string
-}
-
 interface State {
-  info: Info,
   maskIsShow: boolean
 }
 
 export default class Register extends PureComponent<{}, State> {
+  static contextType = StuInfoContext
+
   config: Config = {
     navigationStyle: 'custom'
   }
 
   state = {
-    info: {
-      stu_name: '红小岩',
-      stu_num: '2019213821',
-      stu_qq: '1223898813',
-      stu_phone: '13833528432',
-      department: 'Web研发部'
-    },
     maskIsShow: false
   }
 
@@ -39,23 +28,63 @@ export default class Register extends PureComponent<{}, State> {
     this.setState({ maskIsShow: true })
   }
 
+  maskHide():void {
+    this.setState({ maskIsShow: false })
+  }
+
+  async register(oName: string, dName: string) {
+    const data = {
+      oName,
+      dName
+    }
+    Taro.showLoading({
+      title: '加载中...'
+    })
+    const response = await api.userSubmit(data)
+    Taro.hideLoading()
+    if(response.status === 500) {
+      alert('报名失败')
+    } else if(response.status === 200) {
+      this.maskShow()
+      setTimeout(() => {
+        this.setState({ maskIsShow: true })
+        Taro.redirectTo({
+          url: '/pages/main/index?from=register&to=OrganizationIndex'
+        })
+        Taro.showLoading({
+          title: '加载中...'
+        })
+        setTimeout(() => {
+          Taro.hideLoading()
+        })
+      }, 2000)
+    }
+  }
+
   render() {
+    const stuInfo = this.context
+    const { organization, department } = this.$router.params
+
     return (
       <View className="register">
-        {this.state.maskIsShow ? <Mask type="INFOLACK" /> : null}
-        <Navigation text="信息核对" />
+        {this.state.maskIsShow ? <Mask type="registersuccess" /> : null}
+        <Navigation text="信息核对" enableBack={true} />
         <View>
           <Text>姓名</Text>
-          <Text>{this.state.info.stu_name}</Text>
+          <Text>{stuInfo.stu_name}</Text>
           <Text>学号</Text>
-          <Text>{this.state.info.stu_num}</Text>
+          <Text>{stuInfo.stu_num}</Text>
           <Text>QQ</Text>
-          <Text>{this.state.info.stu_qq}</Text>
+          <Text>{stuInfo.stu_qq}</Text>
           <Text>联系电话</Text>
-          <Text>{this.state.info.stu_phone}</Text>
+          <Text>{stuInfo.stu_phone}</Text>
           <Text>报名部门</Text>
-          <Text>{this.state.info.department}</Text>
-          <Button onClick={this.maskShow}>确认提交</Button>
+          <Text>
+            {organization}-{department}
+          </Text>
+          <Button onClick={() => this.register(organization, department)}>
+            确认提交
+          </Button>
         </View>
       </View>
     )
