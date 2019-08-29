c718ba6ebb95043cb3a047c683b84c9595134dcf
lin0102 <1262280723@qq.com>
2019-08-29 21:41:15 +0800

    增加navigation返回箭头

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,17 +1,25 @@
 import Taro, { PureComponent } from '@tarojs/taro'
-import {View, Text} from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import './index.scss'
 
 interface Props {
-  text: string,
+  text: string
   enableBack: boolean
+  lastPage?: string
 }
 
 export default class Navigation extends PureComponent<Props, {}> {
+  backLastPage() {
+
+  }
+
   render() {
     return (
-      <View className='navigation'>
-        <Text className='navigation-text'>{this.props.text}</Text>
+      <View className="navigation">
+        {this.props.enableBack ? (
+          <View className="arrow" onClick={this.backLastPage} />
+        ) : null}
+        <Text className="navigation-text">{this.props.text}</Text>
       </View>
     )
   }
