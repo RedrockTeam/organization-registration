0cf8ecaa1d2d00fc14cca6f6261f76a0856edd4d
lin0102 <1262280723@qq.com>
2019-09-04 00:31:38 +0800

    去掉默认已报部门

--- src/data/context/HasRegisterContext.ts
+++ src/data/context/HasRegisterContext.ts
@@ -1,47 +1,22 @@
 import Taro from '@tarojs/taro'
 
-const hasRegister = [
-  {
-    organization: '红岩网校工作站',
-    department: 'Web研发部',
-    status: 0,
-    info: [
-      {
-        id: 1,
-        organization: '红岩网校工作站',
-        department: 'Web研发部',
-        info:
-          '亲爱的yjp同学,你好，你先前的申请的社团组织需要进行一面面试，现将详细面试信息发给你',
-        time: '1545096558331'
-      },
-      {
-        id: 2,
-        organization: '红岩网校工作站',
-        department: 'Web研发部',
-        info:
-          '亲爱的yyy同学,你好，你先前的申请的社团组织需要进行一面面试，现将详细面试信息发给你',
-        time: '1566211679355'
-      }
-    ]
-  },
-  {
-    organization: '红岩网校工作站',
-    department: '移动开发部',
-    status: 0,
-    info: []
-  },
-  {
-    organization: '红岩网校工作站',
-    department: '运维安全部',
-    status: 0,
-    info: []
-  },
-  {
-    organization: '红岩网校工作站',
-    department: '产品策划及运营部',
-    status: 0,
-    info: []
-  }
+interface HasRegisterInfo {
+  id: number
+  organization: string
+  department: string
+  info: string
+  time: string
+}
+
+interface HasRegister {
+  organization: string
+  department: string
+  status: number
+  info: Array<HasRegisterInfo>
+}
+
+const hasRegister: Array<HasRegister> = [
+
 ]
 
 const changeStatus = (oName: string, dName: string) => {}
